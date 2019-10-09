6614da3dd2455aae0f290d882c871cba15ed8db7
Vasil Dimov <vd@FreeBSD.org>
2019-10-09 12:30:11 +0200

    fix(core-database-postgres): properly decode vendor_field_hex

--- packages/core-database-postgres/src/migrations/20191003000000-migrate-vendor-field-hex.sql
+++ packages/core-database-postgres/src/migrations/20191003000000-migrate-vendor-field-hex.sql
@@ -1,6 +1,6 @@
 ALTER TABLE transactions
     ALTER COLUMN vendor_field_hex SET DATA TYPE varchar(255)
 	USING
-		ENCODE(('\x' || ENCODE(vendor_field_hex, 'escape'))::bytea, 'escape');
+		CONVERT_FROM(('\x' || ENCODE(vendor_field_hex, 'escape'))::bytea, 'UTF8');
 
 ALTER TABLE transactions RENAME vendor_field_hex TO vendor_field;
